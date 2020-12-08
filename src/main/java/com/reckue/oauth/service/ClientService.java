e82d306144a9f52bf8af5f54ffe91ec961bc61b1
Hardelele <hardelele@yahoo.com>
2020-12-08 23:21:04 +0300

    Change interface to BaseCrudService

--- src/main/java/com/reckue/oauth/service/ClientService.java
+++ src/main/java/com/reckue/oauth/service/ClientService.java
@@ -1,11 +1,11 @@
 package com.reckue.oauth.service;
 
 import com.reckue.oauth.model.Client;
-import com.reckue.oauth.service.interfaces.CrudService;
+import com.reckue.oauth.service.interfaces.BaseCrudService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ClientService implements CrudService<Client> {
+public class ClientService implements BaseCrudService<Client> {
 
     @Override
     public Client create(Client client) {
@@ -14,11 +14,6 @@ public class ClientService implements CrudService<Client> {
 
     @Override
     public Client get(String id) {
-        return null;
-    }
-
-    @Override
-    public Client update(String id, Client client) {
         return null;
     }
 
