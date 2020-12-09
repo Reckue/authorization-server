7fb3d1014213503a6445eff5e6c691c566a8ac61
Hardelele <hardelele@yahoo.com>
2020-12-10 01:44:01 +0300

    Create PasswordCredentialsStoreTest Its unit test case without mocks

---
+++ src/test/java/com/reckue/oauth/cases/store/PasswordCredentialsStoreTest.java
@@ -0,0 +1,44 @@
+package com.reckue.oauth.cases.store;
+
+import com.reckue.oauth.factory.PasswordCredentialsFactory;
+import com.reckue.oauth.factory.base.MongoExampleFactory;
+import com.reckue.oauth.model.PasswordCredentials;
+import com.reckue.oauth.repository.PasswordCredentialsRepository;
+import com.reckue.oauth.service.PasswordCredentialsStoreService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class PasswordCredentialsStoreTest {
+
+    @Autowired
+    private PasswordCredentialsStoreService passwordCredentialsStoreService;
+
+    @Autowired
+    private PasswordCredentialsRepository passwordCredentialsRepository;
+
+    @Autowired
+    private MongoExampleFactory<PasswordCredentials> mongoExampleFactory;
+
+    @Autowired
+    private PasswordCredentialsFactory passwordCredentialsFactory;
+
+    @Test
+    public void createPasswordCredentials() {
+        PasswordCredentials passwordCredentials = passwordCredentialsFactory.produce();
+        long count = passwordCredentialsRepository.count(mongoExampleFactory.produce(passwordCredentials));
+        if (count > 0) {
+            Exception exception = assertThrows(RuntimeException.class, () ->
+                    passwordCredentialsStoreService.create(passwordCredentials)
+            );
+            assertEquals("Password credentials already exists.", exception.getMessage());
+        } else {
+            PasswordCredentials saved = passwordCredentialsStoreService.create(passwordCredentials);
+            assertEquals(passwordCredentials, saved);
+        }
+    }
+}
