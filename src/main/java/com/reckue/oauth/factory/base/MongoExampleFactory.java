1b9a06c34e44877ccb4d1bb9f67c104fb4d08ed3
Hardelele <hardelele@yahoo.com>
2020-12-09 04:09:50 +0300

    Modernize MongoExampleFactory and impl AdditionalPayloadFactory

--- src/main/java/com/reckue/oauth/factory/base/MongoExampleFactory.java
+++ src/main/java/com/reckue/oauth/factory/base/MongoExampleFactory.java
@@ -1,18 +1,30 @@
 package com.reckue.oauth.factory.base;
 
-import com.reckue.oauth.factory.PayloadFactory;
+import com.reckue.oauth.factory.AdditionalPayloadFactory;
 
+import com.reckue.oauth.factory.PayloadFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MongoExampleFactory<T> implements PayloadFactory<Example<T>, T> {
+@RequiredArgsConstructor
+public class MongoExampleFactory<T> implements PayloadFactory<Example<T>, T>, AdditionalPayloadFactory<Example<T>, T> {
 
-    private final ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("id");
+    private final ExampleMatcherFactory exampleMatherFactory;
+
+    @Override
+    public Example<T> produce(T payload, String... ignorePaths) {
+        ExampleMatcher matcher = exampleMatherFactory.produce();
+        for (String path : ignorePaths) {
+            matcher.withIgnorePaths(path);
+        }
+        return Example.of(payload, matcher);
+    }
 
     @Override
     public Example<T> produce(T payload) {
-        return Example.of(payload, matcher);
+        return Example.of(payload, exampleMatherFactory.produce());
     }
 }
