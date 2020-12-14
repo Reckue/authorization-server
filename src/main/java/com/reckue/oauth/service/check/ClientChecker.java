d69412e3bd3c6bad20bbfc6a7716cfdcab749d5d
Hardelele <hardelele@yahoo.com>
2020-12-14 17:33:50 +0300

    Create ClientStoreService and ClientChecker

---
+++ src/main/java/com/reckue/oauth/service/check/ClientChecker.java
@@ -0,0 +1,34 @@
+package com.reckue.oauth.service.check;
+
+import com.reckue.oauth.factory.base.MongoExampleFactory;
+import com.reckue.oauth.model.internal.Client;
+import com.reckue.oauth.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClientChecker implements StoreChecker<Client> {
+
+    private final Logger logger = LoggerFactory.getLogger(PasswordCredentialsChecker.class);
+    private final ClientRepository clientRepository;
+    private final MongoExampleFactory<Client> mongoExampleFactory;
+
+    @Override
+    public boolean exists(Client entity) {
+        Example<Client> example = mongoExampleFactory.produce(entity, "secret");
+        boolean exists = clientRepository.exists(example);
+        logger.info("Exists Client = " + exists);
+        return exists;
+    }
+
+    @Override
+    public void checkAlreadyExists(Client entity) {
+        if (this.exists(entity)) {
+            throw new RuntimeException("Client already exists.");
+        }
+    }
+}
---
+++ src/main/java/com/reckue/oauth/service/store/impl/ClientStoreService.java
@@ -0,0 +1,35 @@
+package com.reckue.oauth.service.store.impl;
+
+import com.reckue.oauth.model.internal.Client;
+import com.reckue.oauth.repository.ClientRepository;
+import com.reckue.oauth.service.check.ClientChecker;
+import com.reckue.oauth.service.store.BaseCrdService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClientStoreService implements BaseCrdService<Client> {
+
+    private final ClientRepository clientRepository;
+    private final ClientChecker clientChecker;
+
+    @Override
+    public Client create(Client client) {
+        clientChecker.checkAlreadyExists(client);
+        return clientRepository.save(client);
+    }
+
+    @Override
+    public Client findById(String id) {
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cannot find client by id"));
+    }
+
+    @Override
+    public Client deleteById(String id) {
+        Client entity = this.findById(id);
+        clientRepository.deleteById(id);
+        return entity;
+    }
+}
