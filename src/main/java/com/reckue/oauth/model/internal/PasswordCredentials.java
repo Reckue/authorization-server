465456e1ee219f54bceb1b670ee747405fa83b53
Hardelele <hardelele@yahoo.com>
2020-12-14 17:36:37 +0300

    Add required annotations

--- src/main/java/com/reckue/oauth/model/internal/PasswordCredentials.java
+++ src/main/java/com/reckue/oauth/model/internal/PasswordCredentials.java
@@ -1,11 +1,13 @@
 package com.reckue.oauth.model.internal;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PasswordCredentials {
