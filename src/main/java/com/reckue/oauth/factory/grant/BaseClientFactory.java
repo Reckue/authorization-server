ce4e588efb7348cf6bdf84f687dac32674f651fa
Hardelele <hardelele@yahoo.com>
2020-12-14 17:38:02 +0300

    Create BaseClientFactory with main client

---
+++ src/main/java/com/reckue/oauth/factory/grant/BaseClientFactory.java
@@ -0,0 +1,12 @@
+package com.reckue.oauth.factory.grant;
+
+import com.reckue.oauth.factory.IndependentFactory;
+import com.reckue.oauth.model.internal.Client;
+
+public class BaseClientFactory implements IndependentFactory<Client> {
+
+    @Override
+    public Client produce() {
+        return new Client("cb92b0c5-6f11-4edd-8b5b-ad4c7d625982", "dfe4c033-da2c-417c-853d-98916dbc7de4");
+    }
+}
