1d8788ad9f826b2c8ae7b9773eee948a2c5764f4
Hardelele <hardelele@yahoo.com>
2020-12-17 00:43:12 +0300

    Create custom salt for reckue oauth

--- src/main/java/com/reckue/oauth/factory/base/PasswordEncoder.java
+++ src/main/java/com/reckue/oauth/factory/base/PasswordEncoder.java
@@ -1,25 +1,25 @@
 package com.reckue.oauth.factory.base;
 
 import com.reckue.oauth.factory.PayloadFactory;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.security.SecureRandom;
 
 @Component
+@RequiredArgsConstructor
 public class PasswordEncoder implements PayloadFactory<String, String> {
+
+    private final ReckueSaltFactory reckueSaltFactory;
 
     @Override
     @SneakyThrows
-    public String produce(String payload) {
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
+    public String produce(String password) {
         MessageDigest md = MessageDigest.getInstance("SHA-512");
-        md.update(salt);
-        byte[] hash = md.digest(payload.getBytes(StandardCharsets.UTF_8));
+        md.update(reckueSaltFactory.produce());
+        byte[] hash = md.digest(password.getBytes(StandardCharsets.UTF_8));
         return new String(hash);
     }
 }
